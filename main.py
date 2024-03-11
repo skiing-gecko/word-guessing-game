72b40428fa109c3cfdd450b1f53d9c20acd79c9b
skiing-gecko <harvey.balaam@durham.ac.uk>
2024-03-11 16:46:08 +0000

    feat: add getter and setter for word_length

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ class Game:
     def __init__(self, word_length: int):
         self._word_length: int = word_length
 
+    # getters and setters
+    @property
+    def word_length(self) -> int:
+        return self._word_length
+
+    @word_length.setter
+    def word_length(self, value: str) -> None:
+        self._word_length = value
+
+    # class methods
     def generate_random_word(self) -> str:
         pass
 
