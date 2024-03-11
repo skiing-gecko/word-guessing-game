9cd12d790f5ef1cdaff2270f591bb4d830ccb795
skiing-gecko <harvey.balaam@durham.ac.uk>
2024-03-11 16:15:49 +0000

    feat: create game class

---
+++ main.py
@@ -0,0 +1,17 @@
+class Game:
+    def __init__(self, word_length: int):
+        self._word_length: int = word_length
+
+    def generate_random_word(self) -> str:
+        pass
+
+    def validate_input(self) -> bool:
+        pass
+
+    def run(self):
+        word: str = self.generate_random_word()
+
+
+if __name__ == "__main__":
+    game = Game(word_length=5)
+    game.run()
