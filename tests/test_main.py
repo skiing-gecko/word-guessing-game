28e68280f899377d83939a29a35a5cd3e02b6f61
skiing-gecko <harvey.balaam@durham.ac.uk>
2024-03-13 14:06:53 +0000

    refactor: use pytest fixture for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,43 +1,44 @@
+import pytest
+
 from main import Game
 
 
-def test_word_too_short():
-    game = Game(word_length=5)
+@pytest.fixture
+def game() -> Game:
+    return Game(word_length=5)
+
+
+def test_word_too_short(game: Game) -> None:
     word = "a" * (game.word_length - 1)
 
     assert game.validate_input(input_word=word) is False
 
 
-def test_word_too_long():
-    game = Game(word_length=5)
+def test_word_too_long(game: Game) -> None:
     word = "a" * (game.word_length + 1)
 
     assert game.validate_input(input_word=word) is False
 
 
-def test_lowercase_word():
-    game = Game(word_length=5)
+def test_lowercase_word(game: Game) -> None:
     word = "a" * game.word_length
 
     assert game.validate_input(input_word=word) is True
 
 
-def test_uppercase_word():
-    game = Game(word_length=5)
+def test_uppercase_word(game: Game) -> None:
     word = "A" * game.word_length
 
     assert game.validate_input(input_word=word) is True
 
 
-def test_numbers_in_word():
-    game = Game(word_length=5)
+def test_numbers_in_word(game: Game) -> None:
     word = "1" * game.word_length
 
     assert game.validate_input(input_word=word) is False
 
 
-def test_symbols_in_word():
-    game = Game(word_length=5)
+def test_symbols_in_word(game: Game) -> None:
     word = "%" * game.word_length
 
     assert game.validate_input(input_word=word) is False
