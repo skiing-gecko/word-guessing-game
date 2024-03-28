4e833d9e39bf422ef4e32947472bbee62bc36323
skiing-gecko <harvey.balaam@durham.ac.uk>
2024-03-28 10:59:26 +0000

    feat: add checks for input letter correctness

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Game:
     def __init__(self, word_length: int):
         self._word_length: int = word_length
+        self._word: str = self.generate_random_word()
 
     # getters and setters
     @property
@@ -13,6 +14,16 @@ class Game:
             raise TypeError("Word length must be an integer")
         self._word_length = value
 
+    @property
+    def word(self) -> str:
+        return self._word
+
+    @word.setter
+    def word(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError("Word word must be a string")
+        self._word = value
+
     # class methods
     def generate_random_word(self) -> str:
         pass
@@ -23,6 +34,16 @@ class Game:
                 # ascii codes for lowercase alphabet, i.e., a-z
                 if 97 <= ord(character.lower()) <= 122:
                     return True
+        return False
+
+    def check_letter_value(self, letter: str) -> bool:
+        if letter in self._word:
+            return True
+        return False
+
+    def check_letter_position(self, letter: str, position: int) -> bool:
+        if self._word[position] == letter:
+            return True
         return False
 
     def run(self) -> None:
